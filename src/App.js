d06c5a81e1f8da2844030c09351c2510b3117fbc
kubryak <al.lcn@yandex.ru>
2023-12-01 20:03:16 +0500

    feat: Реализовать экран при загрузке страницы

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Logo from "./components/logo/logo"
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import vector_left from '../../images/vector-left.svg'
+import vector_right from '../../images/vector-right.svg'
+import logo from '../../images/logo.svg'
+import './logo.css'
+
+export default function Logo() {
+  return (
+    <div className="container">
+      <img className="fade-in-left" src={vector_left} alt="Изображение левого вектора" />
+      <img className="fade-in-right" src={vector_right} alt="Изображение правого вектора" />
+      <img className='logo' src={logo} alt="Логотип компании Veter"/>
+    </div>
+  )
+}
