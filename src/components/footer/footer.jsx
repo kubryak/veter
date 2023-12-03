e6451bee434dc6daf2cd9a0731f98fb8731ca6d7
kubryak <al.lcn@yandex.ru>
2023-12-03 19:10:27 +0500

    fix: Исправить моргание при переключении background

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,8 +4,7 @@ import './footer.css'
 export default function Footer() {
   return (
     <footer className='footer'>
-      <p className='footer__text'>Застройщик ООО “СЗ СУ1-ЗАТОН”. Проектная декларация размещена на сайте
-        <a className='footer__link' target='_blank' href='https://xn--80az8a.xn--d1aqf.xn--p1ai/' rel="noreferrer"> наш.дом.рф</a>
+      <p className='footer__text'>Застройщик ООО “СЗ СУ1-ЗАТОН”. Проектная декларация размещена на сайте <a className='footer__link' target='_blank' href='https://xn--80az8a.xn--d1aqf.xn--p1ai/' rel="noreferrer">наш.дом.рф</a>
         <br />
         <a className='footer__link' target='_blank' href="#" rel="noreferrer">Политика конфиденциальности</a>
       </p>
